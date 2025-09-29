bbe159d655f40f722a9913511f5ca9bea6490b10
ankit-sikarwar-001 <ankitsikarwar681@gmail.com>
2025-09-29 14:36:49 +0530

    nm

--- frontend/src/Components/NewsLetter/Newsletter.jsx
+++ frontend/src/Components/NewsLetter/Newsletter.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import "./Newsletter.css"
+import "./NewsLetter.css"
 const Newsletter = () => {
   return (
     <div className='newsletter'>
-        <h1>Get Exclusive Offers on Your Email</h1>
-        <p>Subscribe to our newsletter and stay updated</p>
-        <div>
-            <input type='email' placeholder='Your Email id'/>
-            <button>Subscribe</button>
-        </div>
+      <h1>Get Exclusive Offers on Your Email</h1>
+      <p>Subscribe to our newsletter and stay updated</p>
+      <div>
+        <input type='email' placeholder='Your Email id' />
+        <button>Subscribe</button>
+      </div>
     </div>
   )
 }
